87d0858f040f72fd62e542cb1f896396fa92d015
mandest <mandest@gmail.com>
2015-03-21 20:11:12 -0500

    Updated set package versions to use CI versioning.

--- scripts/SetPackageVersions.ps1
+++ scripts/SetPackageVersions.ps1
@@ -1,11 +1,8 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 
 @('Nap', 'Nap.Configuration', 'Nap.Html') | %{
-  $version = [System.Reflection.Assembly]::LoadFile("$root\$_\bin\Release\$_.dll").GetName().Version
-  $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-
-  Write-Host "Setting .nuspec for $_ version tag to $versionStr"
+  Write-Host "Setting .nuspec for $_ version tag to $($env:APPVEYOR_BUILD_VERSION)"
   $content = (Get-Content $root\$_\$_.nuspec) 
-  $content = $content -replace '\$version\$',$versionStr
+  $content = $content -replace '\$version\$',$env:APPVEYOR_BUILD_VERSION
   $content | Out-File $root\$_\$_.nuspec
 }
