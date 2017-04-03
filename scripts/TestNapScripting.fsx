e86b39afa7a13d2178114042929590b9a8400ce3
Jonathan Dexter <jdexter@nerdery.com>
2017-04-03 17:13:28 -0500

    Additional testing for types defined in script.

--- scripts/TestNapScripting.fsx
+++ scripts/TestNapScripting.fsx
@@ -1,2 +1,68 @@
-#r "../src/Nap.Immutable/bin/Debug/Nap.Immutable.dll"
-#r "../src/Nap.Html/bin/Debug/Nap.Html.Immutable.dll"
+#r "../src/Nap.Immutable/bin/Release/Nap.Immutable.dll"
+#r "../src/Nap.Html/bin/Release/Nap.Html.Immutable.dll"
+
+open Nap
+open Nap.Html.Attributes
+
+let [<Literal>] page = """
+<!doctype html>
+<html>
+<head>
+    <title>Example Domain</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 50px;
+        background-color: #fff;
+        border-radius: 1em;
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        body {
+            background-color: #fff;
+        }
+        div {
+            width: auto;
+            margin: 0 auto;
+            border-radius: 0;
+            padding: 1em;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>Example Domain</h1>
+    <p>This domain is established to be used for illustrative examples in documents. You may use this
+    domain in examples without prior coordination or asking for permission.</p>
+    <p><a href="http://www.iana.org/domains/example">More information...</a></p>
+</div>
+</body>
+</html>
+"""
+
+type ExamplePage = {
+  [<HtmlElement("h1")>]
+  Heading: string
+  [<HtmlElement("p")>]
+  body: string
+}
+
+let deserialized = (Nap.Html.NapHtmlSerializer()).Deserialize<ExamplePage>(page)
+deserialized |> Option.map (fun d -> d.Heading)
